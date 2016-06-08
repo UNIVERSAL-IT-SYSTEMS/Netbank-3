ba9890ee2602a1fb154e319036992b4b742680ac
Morten Nielsen <morten_richardt@hotmail.com>
2016-06-08 15:53:18 +0200

    login verification

--- src/model/Dao.java
+++ src/model/Dao.java
@@ -1,22 +1,35 @@
 package model;
 
-import java.sql.*;  
+import java.sql.*;
+
+import netbank.Hash;  
 
 public class Dao {
 	public static boolean loginValidate(String name, String pass){  
 		boolean status = false;  
 		try{ 
-			Class.forName("oracle.jdbc.driver.OracleDriver");  
-			Connection con = DriverManager.getConnection(
-					"jdbc:oracle:thin:@localhost:1521:xe","system","oracle");  
-			PreparedStatement ps = con.prepareStatement(
-					"select * from userreg where name=? and pass=?");  
-			ps.setString(1,name);  
-			ps.setString(2,pass);  
+			ResultSet res = servle.getDb().getters("SELECT username,hash,salt FROM \"DTUGRP04\".\"customers\" WHERE username=" + name +";" );
+//			Class.forName("oracle.jdbc.driver.OracleDriver");  
+//			Connection con = DriverManager.getConnection(
+//					"jdbc:oracle:thin:@localhost:1521:xe","system","oracle");  
+//			PreparedStatement ps = con.prepareStatement(
+//					"select * from userreg where name=? and pass=?");  
+//			ps.setString(1,name);  
+//			ps.setString(2,pass);  
+//			
+			if(!res.next()) {
+				return false;
+			}
 			
-			ResultSet rs=ps.executeQuery();  
-			status=rs.next();  
-			
+			if(Hash.SHA512(pass, res.getString(3)) == res.getString(2)) {
+				return true;
+			} else {
+				return false;
+			}
+//			
+//			ResultSet rs=ps.executeQuery();  
+//			status=rs.next();  
+//			
 		}catch(Exception e) {
 			System.out.println(e);
 		}  
